d86671bda0fd0c5011ec2994dff569937d323814
Dustin Graves <dustin@lunarg.com>
2020-06-19 17:22:42 -0600

    Fix code style

--- tools/replay/desktop_main.cpp
+++ tools/replay/desktop_main.cpp
@@ -59,7 +59,8 @@ int main(int argc, const char** argv)
 
     gfxrecon::util::ArgumentParser arg_parser(argc, argv, kOptions, kArguments);
 
-    if (PrintVersion(argv[0], arg_parser)) {
+    if (PrintVersion(argv[0], arg_parser))
+    {
         exit(0);
     }
     else if (arg_parser.IsInvalid() || (arg_parser.GetPositionalArgumentsCount() != 1))
